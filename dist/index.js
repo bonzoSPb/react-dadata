98cfcb6807324d7803a02f3127de1f77e7733126
bonzoSPb <ibestguitarist@gmail.com>
2026-05-27 23:48:35 +0300

    не открывать подсказки при автозаполнении поля

--- dist/index.js
+++ dist/index.js
@@ -52,17 +52,8 @@ var ReactDadata = (function (_super) {
             var value = event.target.value;
             var nativeEvent = event.nativeEvent;
             var inputType = nativeEvent && nativeEvent.inputType;
-            var inputData = nativeEvent && nativeEvent.data;
-            var isSingleEventValueInsert = (inputType === 'insertText'
-                || inputType === 'insertFromPaste')
-                && typeof inputData === 'string'
-                && inputData.length > 1
-                && inputData === value
-                && !_this.state.inputQuery;
             var isAutocompleteInput = _this.props.hideSuggestionsOnAutocomplete
-                && (inputType === 'insertReplacementText'
-                    || isSingleEventValueInsert
-                    || (!inputType && nativeEvent && inputData === null && value.length > 1 && !_this.state.inputQuery));
+                && inputType === 'insertReplacementText';
             if (_this.props.translate) {
                 value = _this.switchLanguage(value);
             }
--- src/index.tsx
+++ src/index.tsx
@@ -221,21 +221,8 @@ export class ReactDadata extends React.PureComponent<ReactDadata.Props, ReactDad
     let value = event.target.value;
     const nativeEvent = event.nativeEvent as any;
     const inputType = nativeEvent && nativeEvent.inputType;
-    const inputData = nativeEvent && nativeEvent.data;
-    const isSingleEventValueInsert = (
-      inputType === 'insertText'
-      || inputType === 'insertFromPaste'
-    )
-      && typeof inputData === 'string'
-      && inputData.length > 1
-      && inputData === value
-      && !this.state.inputQuery;
     const isAutocompleteInput = this.props.hideSuggestionsOnAutocomplete
-      && (
-        inputType === 'insertReplacementText'
-        || isSingleEventValueInsert
-        || (!inputType && nativeEvent && inputData === null && value.length > 1 && !this.state.inputQuery)
-      );
+      && inputType === 'insertReplacementText';
 
     if (this.props.translate) {
       value = this.switchLanguage(value);
